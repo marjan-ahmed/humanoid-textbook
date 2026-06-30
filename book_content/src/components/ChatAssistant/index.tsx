629ada2a67fb4592d228b54202ef04f406e4c78d
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2026-06-30 05:42:16 +0500

    Make close icon bigger, shrink panel width

--- book_content/src/components/ChatAssistant/index.tsx
+++ book_content/src/components/ChatAssistant/index.tsx
@@ -186,8 +186,8 @@ export default function ChatAssistant() {
               </div>
               <div className={styles.panelActions}>
                 <button type="button" className={styles.closeIcon} onClick={() => setIsOpen(false)} aria-label="Close assistant">
-                  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 1L13 13M13 1L1 13" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round"/>
+                  <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 1L13 13M13 1L1 13" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
                   </svg>
                 </button>
               </div>
